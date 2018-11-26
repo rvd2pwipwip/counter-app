9c232320001097d9b4dfc4ef7fa895337e785f7f
Herve Desrosiers <hdesrosiers@stingray.com>
2018-11-26 08:13:44 -0500

    components video 7

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,11 +9,17 @@ class Counter extends Component {
     return count === 0 ? "Zero" : count;
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? `warning` : `primary`;
+    return classes;
+  }
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
