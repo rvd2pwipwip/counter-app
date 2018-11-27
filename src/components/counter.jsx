1f5f7d4f131d0d9a06aa74277fd4dc71fb542c81
Herve Desrosiers <hdesrosiers@stingray.com>
2018-11-27 08:02:30 -0500

    components video 12 conditional rendering, handling events, updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    // tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
   formatCount() {
@@ -15,14 +17,51 @@ class Counter extends Component {
     return classes;
   }
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>Please create a new tag</p>;
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "No tags to render!"}
+        {this.renderTags()}
+      </>
+    );
+  }
+
+  // render() {
+  //   return this.state.tags.length === 0 ? (
+  //     <>
+  //       No tags to render
+  //       <p>Please create a new tag</p>
+  //     </>
+  //   ) : (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 }
 
 export default Counter;
