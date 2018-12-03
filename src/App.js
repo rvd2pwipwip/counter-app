9ffef6e23ed7b1107c2de130fe562bc8be7f4a49
Herve Desrosiers <hdesrosiers@stingray.com>
2018-12-03 08:08:23 -0500

    lifted state to App and added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Counters from "./components/Counters";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 3 }
+    ]
+  };
+
+  handleReset = () => {
+    // 1.copy state counters
+    let counters = [...this.state.counters];
+    // 2.update copy of state
+    counters.map(c => (c.value = 0));
+    // 3.set new counters object to state
+    this.setState({ counters });
+  };
+
+  handleDelete = e => {
+    // 1.copy state counters
+    let counters = [...this.state.counters];
+    // 2.update copy of state
+    counters = counters.filter(c => c !== e);
+    // 3.set new counters object to state
+    this.setState({ counters });
+  };
+
+  handleIncrement = e => {
+    //e is the event argument passed by the onClick of <Counter/> increment button (counter)
+    // 1.copy state counters
+    const counters = [...this.state.counters];
+    // 2.find counter in copied array and increment its value by 1
+    counters.find(c => c === e).value++;
+    // 3.set new counters object to state
+    this.setState({ counters });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Navbar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main role="main" className="container">
+          <Counters
+            handleReset={this.handleReset}
+            handleDelete={this.handleDelete}
+            handleIncrement={this.handleIncrement}
+            counters={this.state.counters}
+          />
+        </main>
+      </>
     );
   }
 }
--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -2,58 +2,21 @@ import React, { Component } from "react";
 import Counter from "./Counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 1 },
-      { id: 3, value: 2 },
-      { id: 4, value: 3 }
-    ]
-  };
-
-  handleReset = () => {
-    // 1.copy state counters
-    let counters = [...this.state.counters];
-    // 2.update copy of state
-    counters.map(c => (c.value = 0));
-    // 3.set new counters object to state
-    this.setState({ counters });
-  };
-
-  handleDelete = e => {
-    // 1.copy state counters
-    let counters = [...this.state.counters];
-    // 2.update copy of state
-    counters = counters.filter(c => c !== e);
-    // 3.set new counters object to state
-    this.setState({ counters });
-  };
-
-  handleIncrement = e => {
-    //e is the event argument passed by the onClick of <Counter/> increment button (counter)
-    // 1.copy state counters
-    const counters = [...this.state.counters];
-    // 2.find counter in copied array and increment its value by 1
-    counters.find(c => c === e).value++;
-    // 3.set new counters object to state
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <>
         <button
           className="btn btn-primary btn-sm m-2 "
-          onClick={this.handleReset}
+          onClick={this.props.handleReset}
         >
           Reset
         </button>
-        {this.state.counters.map(c => (
+        {this.props.counters.map(c => (
           <Counter
             key={c.id}
             counter={c}
-            handleDelete={this.handleDelete}
-            handleIncrement={this.handleIncrement}
+            handleDelete={this.props.handleDelete}
+            handleIncrement={this.props.handleIncrement}
           />
         ))}
       </>
